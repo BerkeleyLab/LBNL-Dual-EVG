49699777fc2d4e3b17c96509d71a4c71611ed49f
Lucas Russo <lerwys@gmail.com>
2022-09-01 15:55:53 -0700

    gateware: add simple debounceFallingEdge testbench

---
+++ gateware/modules/debounceFallingEdge_tb.v
@@ -0,0 +1,78 @@
+`timescale 1ns / 1ns
+module debounceFallingEdge_tb;
+
+reg clk;
+
+integer cc;
+integer errors = 0;
+initial begin
+	if ($test$plusargs("vcd")) begin
+		$dumpfile("debounceFallingEdge.vcd");
+		$dumpvars(5,debounceFallingEdge_tb);
+	end
+
+	clk = 0;
+	for (cc = 0; cc < 5000; cc = cc+1) begin
+		#10; clk = 1;
+		#11; clk = 0;
+	end
+
+	if (errors == 0)
+        $display("PASS");
+end
+
+reg inputActiveLow = 1;
+wire debouncedActiveHigh;
+localparam HIGH_COUNTER_WIDTH = 6;
+debounceFallingEdge #(
+    .HIGH_COUNTER_WIDTH(HIGH_COUNTER_WIDTH))
+  debounceFallingEdge(
+    .clk(clk),
+    .inputActiveLow(inputActiveLow),
+    .debouncedActiveHigh(debouncedActiveHigh));
+
+integer counterInput = 0;
+integer counterDebouncedActiveHigh = 0;
+integer rightAnswer = 0;
+reg fault;
+localparam IDLE             = 2'd0,
+           CNT_EDGES        = 2'd1,
+           CMP_RESULTS      = 2'd2;
+reg [1:0] testState = IDLE;
+always @(posedge clk) begin
+    inputActiveLow <= !(cc == 100 || cc == 200 + (cc % 10));
+
+    case(testState)
+    IDLE: begin
+        if(!inputActiveLow)
+            testState <= CNT_EDGES;
+            counterInput <= 1;
+            counterDebouncedActiveHigh <= 0;
+    end
+
+    CNT_EDGES: begin
+        if (!inputActiveLow) begin
+            counterInput <= counterInput + 1;
+        end
+
+        if (debouncedActiveHigh) begin
+            counterDebouncedActiveHigh <= counterDebouncedActiveHigh + 1;
+        end else begin
+            testState <= CMP_RESULTS;
+        end
+    end
+
+    // expected result is the width of input + 2<<HIGH_COUNTER_WIDTH
+    CMP_RESULTS: begin
+        rightAnswer = counterInput + (1<<HIGH_COUNTER_WIDTH)-1;
+		fault = rightAnswer != counterDebouncedActiveHigh;
+		$display("%x  %x  %s", rightAnswer, counterDebouncedActiveHigh, fault?"FAULT":"    .");
+		if (fault)
+            errors = errors+1;
+
+        testState <= IDLE;
+    end
+    endcase
+end
+
+endmodule
---
+++ gateware/modules/rules.mk
@@ -0,0 +1,35 @@
+VFLAGS_DEP += -I. -y.
+VFLAGS += -I. -y.
+
+TEST_BENCH = debounceFallingEdge_tb
+
+TGT_ := $(TEST_BENCH)
+NO_CHECK =
+CHK_ = $(filter-out $(NO_CHECK), $(TEST_BENCH:%_tb=%_check))
+
+.PHONY: targets checks
+targets: $(TGT_)
+checks: $(CHK_)
+
+CLEAN += $(TGT_) *_tb *.pyc *.bit *.in *.vcd *.lxt *~
+CLEAN_DIRS += _xilinx __pycache__
+
+ifneq (,$(findstring bit,$(MAKECMDGOALS)))
+    ifneq (,$(findstring bits,$(MAKECMDGOALS)))
+	-include $(BITS_:%.bit=$(DEPDIR)/%.bit.d)
+    else
+	-include $(MAKECMDGOALS:%.bit=$(DEPDIR)/%.bit.d)
+    endif
+endif
+ifneq (,$(findstring _tb,$(MAKECMDGOALS)))
+    -include $(MAKECMDGOALS:%_tb=$(DEPDIR)/%_tb.d)
+endif
+ifneq (,$(findstring _view,$(MAKECMDGOALS)))
+    -include $(MAKECMDGOALS:%_tb=$(DEPDIR)/%_tb.d)
+endif
+ifneq (,$(findstring _check,$(MAKECMDGOALS)))
+    -include $(MAKECMDGOALS:%_tb=$(DEPDIR)/%_tb.d)
+endif
+ifeq (,$(MAKECMDGOALS))
+    -include $(TEST_BENCH:%_tb=$(DEPDIR)/%_tb.d)
+endif
