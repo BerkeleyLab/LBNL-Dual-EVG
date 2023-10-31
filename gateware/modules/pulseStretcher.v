58adb57517ef970a2895f86373e6b93e1ad92331
Lucas Russo <lerwys@gmail.com>
2023-10-31 16:57:00 -0700

    modules/pulseStretcher.v: add posedge detection to pulse

--- gateware/modules/pulseStretcher.v
+++ gateware/modules/pulseStretcher.v
@@ -12,7 +12,7 @@ module pulseStretcher #(
     ) (
     input      clk,
     input      rst_a,
-    input      pulse,
+    input      pulse_a,
     output reg pulseStretch = 0);
 
 localparam COUNTER_RELOAD = $rtoi(((CLK_FREQUENCY/1.0e3)*STRETCH_MS)) - 2;
@@ -21,21 +21,37 @@ localparam COUNTER_WIDTH = $clog2(COUNTER_RELOAD+1);
 reg [COUNTER_WIDTH:0] stretchCounter = ~0;
 wire stretchDone = stretchCounter[COUNTER_WIDTH];
 
+(*ASYNC_REG="true"*) reg pulse_m, pulse;
+reg pulse_d, pulsePosedge;
+
 always @(posedge clk or posedge rst_a) begin
     if (rst_a) begin
+        pulse_m <= 0;
+        pulse <= 0;
+
+        pulse_d <= 0;
+        pulsePosedge <= 0;
         stretchCounter <= ~0;
         pulseStretch <= 0;
     end
-    else if (pulse && ((RETRIGGERABLE == "true") ||
-                  (RETRIGGERABLE == "false" && !pulseStretch))) begin
-        stretchCounter <= COUNTER_RELOAD;
-        pulseStretch <= 1;
-    end
-    else if (!stretchDone) begin
-        stretchCounter <= stretchCounter -1;
-    end
     else begin
-        pulseStretch <= 0;
+        pulse_m <= pulse_a;
+        pulse <= pulse_m;
+
+        pulse_d <= pulse;
+        pulsePosedge <= pulse && !pulse_d;
+
+        if (pulsePosedge && ((RETRIGGERABLE == "true") ||
+            (RETRIGGERABLE == "false" && !pulseStretch))) begin
+            stretchCounter <= COUNTER_RELOAD;
+            pulseStretch <= 1;
+        end
+        else if (!stretchDone) begin
+            stretchCounter <= stretchCounter -1;
+        end
+        else begin
+            pulseStretch <= 0;
+        end
     end
 end
 
