49d5110f55a598bdac0dd09ccb9a6f2e829b1ffc
Alessio Amodio <aamodio@lbl.gov>
2024-01-10 16:08:02 -0800

    devg_marble_top.v: new FIFO added to evgLoggers for TLOG command

--- gateware/top/devg_marble/devg_marble_top.v
+++ gateware/top/devg_marble/devg_marble_top.v
@@ -427,8 +427,12 @@ evLogger #(.DEBUG("false"))
   evg1Logger (
     .sysClk(sysClk),
     .GPIO_OUT(GPIO_OUT),
-    .csrStrobe(GPIO_STROBES[GPIO_IDX_EVG_1_LOG_CSR]),
-    .status(GPIO_IN[GPIO_IDX_EVG_1_LOG_CSR]),
+    .csrStrobe(GPIO_STROBES[GPIO_IDX_EVG_1_DISP_LOG_CSR]),
+    .status(GPIO_IN[GPIO_IDX_EVG_1_DISP_LOG_CSR]),
+    .csrStrobeAux(GPIO_STROBES[GPIO_IDX_EVENT_LOG_1_CSR]),
+    .statusAux(GPIO_IN[GPIO_IDX_EVENT_LOG_1_CSR]),
+    .sysDataTicks(GPIO_IN[GPIO_IDX_EVENT_LOG_1_TICKS]),
+    .debug_led(debug_led),
     .evgTxClk(evg1TxClk),
     .evgTxData(evg1TxData),
     .evgTxCharIsK(evg1TxCharIsK));
@@ -523,8 +527,11 @@ evLogger #(.DEBUG("false"))
   evg2Logger (
     .sysClk(sysClk),
     .GPIO_OUT(GPIO_OUT),
-    .csrStrobe(GPIO_STROBES[GPIO_IDX_EVG_2_LOG_CSR]),
-    .status(GPIO_IN[GPIO_IDX_EVG_2_LOG_CSR]),
+    .csrStrobe(GPIO_STROBES[GPIO_IDX_EVG_2_DISP_LOG_CSR]),
+    .status(GPIO_IN[GPIO_IDX_EVG_2_DISP_LOG_CSR]),
+    .csrStrobeLogger2(GPIO_STROBES[GPIO_IDX_EVG_2_TLOG_CSR]),
+    .statusLogger2(GPIO_IN[GPIO_IDX_EVG_2_TLOG_CSR]),
+    .sysDataTicks(GPIO_IN[GPIO_IDX_EVG_2_TLOG_TICKS]),
     .evgTxClk(evg2TxClk),
     .evgTxData(evg2TxData),
     .evgTxCharIsK(evg2TxCharIsK));
