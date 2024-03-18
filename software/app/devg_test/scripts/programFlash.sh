20c809ebc623452583f14c0d219c052eaf885d20
Lucas Russo <lerwys@gmail.com>
2024-03-18 15:04:32 -0700

    software: fix bistream names in programFlash.sh script

--- software/app/devg_test/scripts/programFlash.sh
+++ software/app/devg_test/scripts/programFlash.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 IP="${HSD_IP_ADDRESS=131.243.196.245}"
-SRC="BOOT.bin"
+SRC="DEVG_A.bit"
 
 for i
 do
     case "$i" in
-        *.bin) SRC="$i" ;;
+        *.bit) SRC="$i" ;;
         *)     IP="$i" ;;
     esac
 done
 
 set -ex
 test -r "$SRC"
-tftp -v -m binary "$IP" -c put "$SRC" BOOT.BIN
+tftp -v -m binary "$IP" -c put "$SRC" DEVG_A.bit
 sleep 5
-tftp -v -m binary "$IP" -c get BOOT.BIN BOOTchk.bin
+tftp -v -m binary "$IP" -c get DEVG_A.bit DEVG_Achk.bit
 set +e
-if cmp "$SRC" BOOTchk.bin
+if cmp "$SRC" DEVG_Achk.bit
 then
     echo "Flash write succeeded."
-    rm BOOTchk.bin
+    rm DEVG_Achk.bit
 else
     echo "Flash write may have failed!"
 fi
