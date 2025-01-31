b8f39f6a83bb4e9ebb8cc1757b04367684d62f49
Lucas Russo <lerwys@gmail.com>
2025-01-31 11:18:55 -0800

    scripts/deploy.sh: fix script to add EXTRA_OPTS to openocd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,11 @@
 set -euo pipefail
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
-BITFILE=${1:-download.bit}
+EXTRA_OPTS=${EXTRA_OPTS}
+BITFILE=${BITFILE:-devg_marble.bit}
 
 if test -r "${BITFILE}"; then
-    openocd -s "${SCRIPTPATH}" -f marble.cfg -c "transport select jtag; init; xc7_program xc7.tap; pld load 0 ${BITFILE}; exit"
+    openocd -s "${SCRIPTPATH}" -f marble.cfg -c "${EXTRA_OPTS}" -c "transport select jtag; init; xc7_program xc7.tap; pld load 0 ${BITFILE}; exit"
 else
     echo "${BITFILE} not found"
 fi
