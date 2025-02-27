ba47954c09fd752d5bcd20ee7f1b5935e4d96c07
Lucas Russo <lerwys@gmail.com>
2025-02-27 15:14:15 -0800

    software/target: fix F1 hearbeat and BR/AR alignment counts

--- software/target/devg_marble/config.h
+++ software/target/devg_marble/config.h
@@ -19,8 +19,8 @@
 #define CFG_EVG2_CLOCK_PER_ARSR_COINCIDENCE 3116
 
 // FIXME: Alternate values to work with ALS SR
-#define CFG_EVG1_CLK_PER_BR_AR_ALIGNMENT    10250
-#define CFG_EVG1_CLK_PER_HEARTBEAT          124640000
+#define CFG_EVG1_CLK_PER_BR_AR_ALIGNMENT    50616
+#define CFG_EVG1_CLK_PER_HEARTBEAT          126540000
 
 // Use FMC for 115 MGT quad. 0 = FMC, 1 = QSFP 2
 #define CFG_MGT_FMC_OR_QSFP                0
