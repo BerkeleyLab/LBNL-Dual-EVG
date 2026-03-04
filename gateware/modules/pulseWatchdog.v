450b649d430ae24082642a449ac9e93512984c65
Lucas Russo <lerwys@gmail.com>
2026-03-04 15:03:21 -0800

    modules: move watchdog to pulse input

--- gateware/modules/pulseWatchdog.v
+++ gateware/modules/pulseWatchdog.v
@@ -1,21 +1,32 @@
-module markerWatchdog #(
+module pulseWatchdog #(
+    // In Hz
     parameter SYSCLK_FREQUENCY = 100000000,
+    // In Hz
+    parameter PULSE_FREQUENCY  = 1,
     parameter DEBUG            = "false"
     ) (
+    input      clk,
+    input      pulseIn,
+
     input      sysClk,
-    input      markerIn,
     output reg isValid = 0);
 
-localparam UPPER_LIMIT = ((SYSCLK_FREQUENCY * 11) / 10);
-localparam LOWER_LIMIT = ((SYSCLK_FREQUENCY *  9) / 10);
-(*mark_debug=DEBUG*) reg [$clog2(LOWER_LIMIT+1)-1:0] watchdog;
-(*mark_debug=DEBUG*) reg marker_m, marker, marker_d;
+localparam SYSCLK_COUNT = (SYSCLK_FREQUENCY + PULSE_FREQUENCY - 1)/ PULSE_FREQUENCY;
+localparam UPPER_LIMIT = ((SYSCLK_COUNT * 11) / 10);
+localparam LOWER_LIMIT = ((SYSCLK_COUNT *  9) / 10);
+(*mark_debug=DEBUG*) reg [$clog2(UPPER_LIMIT+1)-1:0] watchdog;
+
+wire pulse;
+
+pulseSync
+  pulseSync (
+    .s_clk(clk),
+    .s_pulse(pulseIn),
+    .d_clk(sysClk),
+    .d_pulse(pulse));
 
 always @(posedge sysClk) begin
-    marker_m <= markerIn;
-    marker   <= marker_m;
-    marker_d <= marker;
-    if (marker && !marker_d) begin
+    if (pulse) begin
         watchdog <= 0;
         if ((watchdog > LOWER_LIMIT)
          && (watchdog < UPPER_LIMIT)) begin
