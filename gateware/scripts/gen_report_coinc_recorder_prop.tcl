ebb90192240f2a39b31e1e97e2e7a4c235fba972
Lucas Russo <lerwys@gmail.com>
2026-03-06 10:21:16 -0800

    scripts/gen_report_coinc_recorder_prop.tcl: fix script to check for correct LOC

--- gateware/scripts/gen_report_coinc_recorder_prop.tcl
+++ gateware/scripts/gen_report_coinc_recorder_prop.tcl
@@ -1,28 +1,47 @@
-proc gen_report_coinc_recorder_prop {xpr_file report_coinc_recorder_prop_file} {
+proc gen_report_coinc_recorder_prop {xpr_file} {
+
+    # Define list of arrays for cells
+    set cells [list \
+        [dict create name "coincidenceRecorder1/genblk2\[0\].value_a_reg" loc "SLICE_X86Y182"] \
+        [dict create name "coincidenceRecorder1/genblk2\[0\].value_m_reg" loc "SLICE_X87Y182"] \
+        [dict create name "coincidenceRecorder2/genblk2\[0\].value_a_reg" loc "SLICE_X86Y181"] \
+        [dict create name "coincidenceRecorder2/genblk2\[0\].value_m_reg" loc "SLICE_X87Y181"]
+    ]
 
     open_project $xpr_file
     open_run impl_1
-    report_property -append \
-        -file $report_coinc_recorder_prop_file \
-        [get_cells -hier -filter {NAME =~ *coincidenceRecorder1/genblk2[0].value_a_reg}]
-    report_property -append \
-        -file $report_coinc_recorder_prop_file \
-        [get_cells -hier -filter {NAME =~ *coincidenceRecorder1/genblk2[0].value_m_reg}]
-    report_property -append \
-        -file $report_coinc_recorder_prop_file \
-        [get_cells -hier -filter {NAME =~ *coincidenceRecorder2/genblk2[0].value_a_reg}]
-    report_property -append \
-        -file $report_coinc_recorder_prop_file \
-        [get_cells -hier -filter {NAME =~ *coincidenceRecorder2/genblk2[0].value_m_reg}]
+
+    foreach cell $cells {
+        set name [dict get $cell name]
+        set expected_loc [dict get $cell loc]
+
+        puts "Checking placement for: $name"
+
+        set cell_obj [get_cells -quiet -hier -filter "NAME =~ *$name"]
+
+        if {$cell_obj eq ""} {
+            puts "ERROR: Cell \"$name\" was not found in the design!"
+            exit 3
+        }
+
+        set actual_loc [get_property LOC $cell_obj]
+        set is_fixed [get_property IS_LOC_FIXED $cell_obj]
+
+        if {$actual_loc eq $expected_loc} {
+            puts "SUCCESS: cell \"$name\" is correctly placed on LOC \"$actual_loc\""
+        } else {
+            puts "FAILURE: cell \"$name\" is placed on \"$actual_loc\", but expected \"$expected_loc\""
+            exit 3
+        }
+    }
 }
 
-if { $argc < 2 } {
+if { $argc < 1 } {
     puts "Not enough arguments"
-    puts "Usage: vivado -mode batch -nojou -nolog -source gen_report_coinc_recorder_prop.tcl -tclargs <xpr_file> <report_coinc_recorder_prop_file>"
+    puts "Usage: vivado -mode batch -nojou -nolog -source gen_report_coinc_recorder_prop.tcl -tclargs <xpr_file>"
     exit
 }
 
 set xpr_file [lindex $argv 0]
-set report_coinc_recorder_prop_file [lindex $argv 1]
 
-gen_report_coinc_recorder_prop $xpr_file $report_coinc_recorder_prop_file
+gen_report_coinc_recorder_prop $xpr_file
