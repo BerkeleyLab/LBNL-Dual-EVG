298d25f16f9714f94f262143df0dc6f6fab5c21f
Lucas Russo <lerwys@gmail.com>
2026-07-09 11:12:15 -0700

    modules/bantamweightUDP_rules.mk: fix typo on closing parentheses

--- gateware/modules/bantamweightUDP_rules.mk
+++ gateware/modules/bantamweightUDP_rules.mk
@@ -7,7 +7,7 @@ bantamweightUDP_TARGET = _gen/bantamweightUDP
 IP_CORES_DIRS += $(bantamweightUDP_DIR)
 
 bantamweightUDP: $(bantamweightUDP_SRCS)
-	cp $(bantamweightUDP_SRCS $($@_TARGET)
+	cp $(bantamweightUDP_SRCS) $($@_TARGET)
 	touch $@
 
 CLEAN_DIRS += $(bantamweightUDP_TARGET)
